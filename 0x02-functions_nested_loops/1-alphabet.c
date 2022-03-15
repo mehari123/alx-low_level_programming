39f4497005aa7edb48298d9005b6bf2b9a7f75d8
mehari123 <meharithegreat@gmail.com>
2022-03-15 16:37:16 +0300

    add

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - ihis function will print alphaber
+ * return -void
+ */
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha < 'A')
+	{
+		/*
+		_putchar(alpha);
+		alpha++;
+		*/
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - it callse all funciton to run
+ *
+ * return - it tetuns intiger
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
