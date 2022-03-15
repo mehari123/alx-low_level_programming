89b16776d262c96b99d74f831722c3793997d7ac
mehari123 <meharithegreat@gmail.com>
2022-03-15 15:28:56 +0300

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  *main - Prints Holberton as a message.
+  *this main funcion will print "_putchar" wiht out
+  *using printf and put functions
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
