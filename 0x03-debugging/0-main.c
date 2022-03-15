ad299f4c11fe14b2c65091602bab79d556945d5e
mehari123 <meharithegreat@gmail.com>
2022-03-15 21:55:24 +0300

    add :

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - it callse all funciton to run
+ *
+ * return - it tetuns intiger
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+this files are used to debug
