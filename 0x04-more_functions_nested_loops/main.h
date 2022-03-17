e9f0316608f99056c7f8d9503cb814da7468d1e4
mehari123 <meharithegreat@gmail.com>
2022-03-17 21:19:50 +0300

    add

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,17 @@
-#ifdef _main_h_
-#define _main_h_
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
-int _putchar(char d);
-int _isupper(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
