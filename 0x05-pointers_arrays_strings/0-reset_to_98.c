32b2c84ae91eae8c7baf3d8b0ad549e2e4cd57f7
mehari123 <meharithegreat@gmail.com>
2022-03-21 14:17:50 +0300

    add

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,12 @@
 
 /**
  * reset_to_98- it points and change the value
+ * @n: - it is argument to be recivied as reference
  */
 void reset_to_98(int *n)
 {
+	/**
+	 * n - us updating to value
+	 */
 	*n = 98;
 }
