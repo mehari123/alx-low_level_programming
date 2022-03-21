515e0153c2f2d07909039b2997d4c70a3e528383
mehari123 <meharithegreat@gmail.com>
2022-03-21 14:10:09 +0300

    number

--- 0x05-pointers_arrays_strings/main.g
+++
@@ -1,6 +0,0 @@
-ifdef _main_h_
-define _main_h_
-
-void reset_to_98(int *n);
-
-endif
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
-ifdef _main_h_
-define _main_h_
+#ifndef _main_h_
+#define _main_h_
 
 void reset_to_98(int *n);
 
-endif
+#endif
